613e35deeaaedb2fa02d26e90330919c3d9efdcf
student <student@ucf.com>
2016-03-25 16:48:13 -0400

    new base classes

---
+++ MoMoker/PointerMocker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoMoker
+{
+    class PointerMocker
+    {
+        public void MovePointerTo(Coordinate coordinate){
+
+        }
+
+        public void DoPointerAction()
+        {
+
+        }
+    }
+}
