ca21b7e1af9f3685e252db2e5434699fbd731cc3
armin <armin@armin.pc>
2016-03-31 18:15:20 +0430

    new stuff

--- MoMoker/src/MouseController.cs
+++ MoMoker/src/MouseController.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MoMoker
 {
-    class Program
+    public static class MouseController
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
---
+++ MoMoker/src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoMoker
+{
+    class Program
+    {
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+        static void Main(string[] args)
+        {
+            SetCursorPos(199, 199);
+            for (int i = 0; i < 500; i++)
+            {
+                SetCursorPos(i, i);
+            }
+        }
+    }
+}
