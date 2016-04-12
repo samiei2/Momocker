78734aca4fb4027553ab9b2a4708e15643907f8e
armin <armin@armin.pc>
2016-04-12 20:27:07 +0430

    window screen capture

---
+++ TransparentController/Win32.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransparentController
+{
+    class Win32
+    {
+        #region WIN32
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out System.Drawing.Point lpPoint);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetWindowText(IntPtr hWnd, string lpString);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr WindowFromPoint(System.Drawing.Point p);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+        [DllImport("gdi32.dll")]
+        public static extern bool BitBlt(IntPtr hdcDest, int xDest, int yDest, int
+        wDest, int hDest, IntPtr hdcSource, int xSrc, int ySrc, CopyPixelOperation rop);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr DeleteDC(IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr DeleteObject(IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr SelectObject(IntPtr hdc, IntPtr bmp);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr ptr);
+        
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetActiveWindow();
+        #endregion
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        public static Size GetControlSize(IntPtr hWnd)
+        {
+            RECT pRect;
+            Size cSize = new Size();
+            // get coordinates relative to window
+            GetWindowRect(hWnd, out pRect);
+
+            cSize.Width = pRect.Right - pRect.Left;
+            cSize.Height = pRect.Bottom - pRect.Top;
+
+            return cSize;
+        }
+
+        // Upper left corner
+        public static Point GetControlPosition(IntPtr hWnd)
+        {
+            RECT pRect;
+            Point p = new Point();
+            // get coordinates relative to window
+            GetWindowRect(hWnd, out pRect);
+
+            p.X = pRect.Left;
+            p.Y = pRect.Top;
+
+            return p;
+        }
+
+        public static IntPtr GetWindowHandleFromCursor()
+        {
+            System.Drawing.Point p;
+            if (GetCursorPos(out p))
+            {
+                IntPtr hWnd = WindowFromPoint(p);
+                if (hWnd != IntPtr.Zero)
+                {
+                    //SetWindowText(hWnd, "Found You: " + p.X + "," + p.Y);
+                }
+                return hWnd;
+            }
+            return IntPtr.Zero;
+        }
+    }
+}
---
+++ TransparentController/WindowOverlay.xaml.cs
@@ -0,0 +1,189 @@
+﻿using Gma.System.MouseKeyHook;
+using MinimizeCapture;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TransparentController
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class WindowOverlay : Window
+    {
+        System.Timers.Timer _timer = new System.Timers.Timer();
+        private IKeyboardMouseEvents m_Events;
+        private IntPtr _selectedWindow = IntPtr.Zero;
+
+        public WindowOverlay()
+        {
+            InitializeComponent();
+            Subscribe(Hook.GlobalEvents());
+            _timer.AutoReset = true;
+            _timer.Interval = 1000;
+            _timer.Elapsed += TimedScreenShot;
+            _timer.Start();
+        }
+
+        private void TimedScreenShot(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (_selectedWindow != IntPtr.Zero)
+            {
+                Dispatcher.Invoke((Action)(() => { image.Source = null; }));
+                var bitmap = WindowSnap.GetWindowSnap(_selectedWindow, true).Image;
+                Dispatcher.Invoke((Action)(() => { image.Source = BitmapToImageSource(bitmap); }));
+            }
+        }
+
+        private void SwitchPic(BitmapImage img)
+        {
+            img.Freeze();
+            if (!img.Dispatcher.CheckAccess())
+            {
+                Dispatcher.BeginInvoke((Action)(() => {
+                    image.Source = img;
+                }),img);
+            }
+            else
+            {
+                image.Source = img;
+            }
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            //var hwnd = new WindowInteropHelper(this).Handle;
+            //WindowsServices.SetWindowExTransparent(hwnd);
+        }
+
+        
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Win32.GetWindowHandleFromCursor();
+        }
+
+        private void Subscribe(IKeyboardMouseEvents events)
+        {
+            m_Events = events;
+            m_Events.MouseDown += OnMouseDown;
+            m_Events.MouseMove += OnMouseMove;
+            m_Events.MouseDragFinished += OnMouseDragFinished;
+        }
+
+        private void Unsubscribe()
+        {
+            if (m_Events == null) return;
+            
+
+            m_Events.Dispose();
+            m_Events = null;
+        }
+
+        private void OnMouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            _selectedWindow = Win32.GetForegroundWindow();
+            //image.Source = null;
+            //var window = Win32.GetForegroundWindow();
+            //if (window != IntPtr.Zero) 
+            //{
+            //    ScreenCapture.CaptureWindow(window);
+            //    var bitmap = WindowSnap.GetWindowSnap(window, true).Image;
+                //var bitmap = ScreenCapture.CaptureDesktop();
+                //image.Source = BitmapToImageSource(bitmap);
+                //image.InvalidateVisual();
+                //System.Drawing.Point topLeft = Win32.GetControlPosition(window);
+                //System.Drawing.Size size = Win32.GetControlSize(window);
+                //this.Left = topLeft.X;
+                //this.Top = topLeft.Y;
+                //this.Height = size.Height;
+                //this.Width = size.Width;
+                //this.Show();
+            //}
+            //test();
+            //var window = GetWindowHandle();
+            //if (window != IntPtr.Zero)
+            //{
+            //    var bitmap = WindowSnap.GetWindowSnap(window, true).Image;
+            //    //var bitmap = ScreenCapture.CaptureDesktop();
+            //    image.Source = BitmapToImageSource(bitmap);
+            //}
+        }
+
+        private void OnMouseDragFinished(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            var window = Win32.GetActiveWindow();
+            if (window != IntPtr.Zero)
+            {
+                System.Drawing.Point topLeft = Win32.GetControlPosition(window);
+                System.Drawing.Size size = Win32.GetControlSize(window);
+                this.Left = topLeft.X;
+                this.Top = topLeft.Y;
+                this.Height = size.Height;
+                this.Width = size.Width;
+                this.Show();
+            }
+        }
+        
+        private void test()
+        {
+            System.Drawing.Size sz = Screen.PrimaryScreen.Bounds.Size;
+            IntPtr hDesk = Win32.GetDesktopWindow();
+            IntPtr hSrce = Win32.GetWindowDC(hDesk);
+            IntPtr hDest = Win32.CreateCompatibleDC(hSrce);
+            IntPtr hBmp = Win32.CreateCompatibleBitmap(hSrce, sz.Width, sz.Height);
+            IntPtr hOldBmp = Win32.SelectObject(hDest, hBmp);
+            bool b = Win32.BitBlt(hDest, 0, 0, sz.Width, sz.Height, hSrce, 0, 0, CopyPixelOperation.SourceCopy | CopyPixelOperation.CaptureBlt);
+            Bitmap bmp = Bitmap.FromHbitmap(hBmp);
+            Win32.SelectObject(hDest, hOldBmp);
+            Win32.DeleteObject(hBmp);
+            Win32.DeleteDC(hDest);
+            Win32.ReleaseDC(hDesk, hSrce);
+            //bmp.Save(@"c:\temp\test.png");
+            image.Source = null;
+            image.Source = BitmapToImageSource(bmp);
+            bmp.Dispose();
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private void OnMouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            //textBox.Text = e.X + "," + e.Y;
+            //test();
+        }
+    }
+}
